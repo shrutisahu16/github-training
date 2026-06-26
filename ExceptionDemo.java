7af5875f6f3ea62a50328768daff9da6527d4dfe
darsh-mhjn <darshamitmahajan@gmail.com>
2026-06-26 20:40:14 +0530

    Modified Exception Demo Class

--- ExceptionDemo.java
+++ ExceptionDemo.java
@@ -1,3 +1,23 @@
+import java.util.Scanner;
+
+class Emp{
+    String name;
+    int age;
+    void getDetails() throws AgeException
+    {
+        System.out.println("Enter your Name");
+        Scanner sc = new Scanner(System.in);
+        name=sc.next();
+        System.out.println("Enter your age: ");
+        age=sc.nextInt();
+        if(age<16)
+        {
+            throw new AgeException(age);
+        }
+    }
+}
+
+
 class ExceptionDemo {
     public static void main(String[] args) {
         try {
